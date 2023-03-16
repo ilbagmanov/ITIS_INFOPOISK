2caf8783b3b091d95ad6608b2d9f2e5404f204f0
Tuhvatullin Bulat <94221495+tuhvbulat@users.noreply.github.com>
2023-03-17 00:37:01 +0300

    Update preparation.py

--- hw3/preparation.py
+++ hw3/preparation.py
@@ -1,36 +1,31 @@
 from collections import defaultdict
 import os
 
-DIRECTORY = 'clean'
-LEMASS = 'lemmas'
-TXT = '.txt'
-UTF8 = "utf=8"
-_UTF8 = "utf-8"
-_N = '\n'
+DIRECTORY = 'Выкачка_очищенная'
+
+def get_lemmas(text):
+    return [token.split(':')[0] for token in text.split()]
 
 def get_inverted_index():
     term_documents_dict = defaultdict(list)
     idx = 0
     for root, dirs, files in os.walk(DIRECTORY):
+        print(files)
         for file in files:
-            if file.lower().endswith(TXT) and file.lower().startswith(LEMASS):
+            if file.lower().endswith('.txt') and file.lower().startswith('lemmas'):
                 idx += 1
-                path_file = os.path.join(root, file)
-                with open(path_file, encoding=UTF8) as f:
-                    lemmas = list(map(lambda x: x.split(':')[0], f.readlines()))
+                with open(os.path.join(root, file), encoding='utf-8') as f:
+                    lemmas = get_lemmas(f.read())
                 for lemma in lemmas:
                     term_documents_dict[lemma].append(idx)
     return term_documents_dict
 
-
 if __name__ == '__main__':
-    td_dict = get_inverted_index()
-    with open('inverted_index.txt', 'w', encoding=_UTF8) as f:
-        for k, v in td_dict.items():
-            f.write(k + ' ' + ' '.join(map(str, v)) + _N)
-    count_inverted_word = []
-    for k, v in td_dict.items():
-        count_inverted_word.append({"count": len(v), "inverted_array": v, "word": k})
-    with open('inverted_index_2.txt', 'w', encoding=_UTF8) as f:
-        for ciw in count_inverted_word:
-            f.write(str(ciw) + _N)
+    inverted_index = get_inverted_index()
+    with open('inverted_index.txt', 'w', encoding='utf-8') as f:
+        for term, docs in inverted_index.items():
+            f.write(f"{term} {' '.join(map(str, docs))}\n")
+    inverted_index_info = [{'count': len(docs), 'inverted_array': docs, 'word': term} for term, docs in inverted_index.items()]
+    with open('inverted_index_2.txt', 'w', encoding='utf-8') as f:
+        for term_info in inverted_index_info:
+            f.write(str(term_info) + '\n')
